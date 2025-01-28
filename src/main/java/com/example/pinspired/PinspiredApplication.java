2ce7e507a7d746bb49d11032388884076445d78b
enisarifi <103397859+enisarifi@users.noreply.github.com>
2025-01-28 16:39:02 +0100

    first commit

---
+++ src/main/java/com/example/pinspired/PinspiredApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinspired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinspiredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinspiredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pinspired/PinspiredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pinspired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinspiredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
